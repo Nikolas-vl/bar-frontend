c6508b65f46ed892497db8dea474520ac498e179
Nikolas-vl <nikvv02@gmail.com>
2026-03-25 12:02:17 +0100

    fixed build errors

--- src/features/cart/hooks/useCart.tsx
+++ src/features/cart/hooks/useCart.tsx
@@ -2,11 +2,12 @@ import { useQuery } from '@tanstack/react-query';
 import { cartApi } from '@/shared/lib/api/cart.api';
 import { queryKeys } from '@/shared/lib/utils/queryKeys';
 import { useAuthStore } from '@/app/store/auth.store';
+import type { Cart } from '@/shared/types';
 
 export const useCart = () => {
   const isAuthenticated = useAuthStore(s => s.isAuthenticated);
 
-  return useQuery({
+  return useQuery<Cart>({
     queryKey: queryKeys.cart.me,
     queryFn: cartApi.getCart,
     enabled: isAuthenticated,
